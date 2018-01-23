765d71ded18156f29018729269ff90db143f07cd
swatich <swatiying@gmail.com>
2018-01-23 15:50:42 +0530

    initial commit

--- app/src/main/java/motobeans/com/todolistexample/Adapters/Holders/DeliveryItemHolder.kt
+++ app/src/main/java/motobeans/com/todolistexample/Adapters/Holders/DeliveryItemHolder.kt
@@ -2,7 +2,9 @@ package motobeans.com.floweraura_deliveryapp_kotlin.MVP.View.Adapters.Holders
 
 import android.content.Context
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
 import android.support.v7.widget.RecyclerView
+import motobeans.com.todolistexample.R
 import motobeans.com.todolistexample.databinding.ItemTaskBinding
 import motobeans.com.todolistexample.persistence.Model.Task
 import motobeans.com.todolistexample.ui.AddUpdateTaskActivity
@@ -15,6 +17,7 @@ class DeliveryItemHolder(val context: Context,
     val binding: ItemTaskBinding, val taskViewModel: TaskViewModel) : RecyclerView.ViewHolder(binding.root) {
 
   fun handleCard(position: Int, itemData: Task) {
+    binding.tvTaskTitle.text = itemData.taskTitle
     binding.tvTaskDesc.text = itemData.taskDesc
 
     if(itemData.isActive) {
@@ -29,7 +32,24 @@ class DeliveryItemHolder(val context: Context,
       itemData.isActive = !itemData.isActive
       taskViewModel.save(task = itemData)
     }
-    binding.tvDelete.setOnClickListener { _ -> taskViewModel.deleteTaskById(id = itemData.id)}
+    binding.tvEdit.setOnClickListener { _ -> AddUpdateTaskActivity.start(context, itemData.id) }
+    binding.tvDelete.setOnClickListener { _ -> deleteProduct(itemData.id)}
     binding.root.setOnClickListener { _ -> AddUpdateTaskActivity.start(context, itemData.id) }
+  }
+
+  private fun deleteProduct(itemId: String) {
+    val builder = AlertDialog.Builder(context, R.style.AppCompatAlertDialogStyle)
+    builder.setTitle("Delete!")
+    builder.setMessage("Do you really want to Delete this task ?")
+    builder.setPositiveButton(android.R.string.yes, { dialog, _ ->
+      taskViewModel.deleteTaskById(id = itemId)
+      dialog.dismiss()
+    })
+    builder.setNegativeButton(android.R.string.cancel, { dialog, _ ->
+      dialog.dismiss()
+    })
+    val dialog = builder.create()
+
+    dialog.show()
   }
 }
